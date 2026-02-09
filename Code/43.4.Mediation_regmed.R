54b757bf3f6b00b84d3e66e0f3a9b9bb58186fc2
Maria Sopena Rios <mariasopenar@gmail.com>
2026-02-09 12:09:17 +0100

    fixed bug

--- Code/43.4.Mediation_regmed.R
+++ Code/43.4.Mediation_regmed.R
@@ -354,7 +354,7 @@ for (trait in individual_variables) {
   genes_symbol <- gene_annotation[rownames(expr_residuals),'symbol']
   #we have duplicated symbols therefore we will remove one 
   expr_residuals$gene_symbol <- genes_symbol
-  expr_residuals <- expr_residuals[!duplicated(expr_residuals$genes_symbol)]
+  expr_residuals <- expr_residuals[!duplicated(expr_residuals$gene_symbol),]
   rownames(expr_residuals) <- expr_residuals$genes_symbol
   expr_residuals <- expr_residuals[,-which(colnames(expr_residuals) == "gene_symbol")]
   
