2f0e0a36b32786cb81c164d4733f6d686f0c6f36
Maria Sopena Rios <mariasopenar@gmail.com>
2026-03-13 12:39:57 +0100

    update bug

--- Code/43.4.Mediation_regmed.R
+++ Code/43.4.Mediation_regmed.R
@@ -408,8 +408,7 @@ ncores <- detectCores() - 1
 g.lm_models <- pbmclapply(
   genes,
   function(g) lm.cis_models(
-    g, meth_residuals, expr_residuals,metadata, gene_variants.list,trait),mc.cores = ncores,  mc.style = "ETA",
-  file = stderr())
+    g, meth_residuals, expr_residuals,metadata, gene_variants.list,trait),mc.cores = ncores,  mc.style = "ETA")
 names(g.lm_models) <-  genes
 # genes that  cannot modelled ----
 #d[["Gene:NotModelled"]] <- sum(is.na(g.lm_models))
